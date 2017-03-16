db8a47bdd072da9d4eff1c2f5c1f5bc1dbeb6d94
Jungho Kim <jk@architech.ca>
2017-03-15 20:01:04 -0400

    refactored

--- app/index.js
+++ app/index.js
@@ -5,23 +5,23 @@ var mongodb = require('mongodb');
 var os = require('os');
 
 var app = express();
-var url = 'mongodb://' + 
-    config.get('Env.database.host') + ':' + 
-    config.get('Env.database.port') + '/' + 
+var url = 'mongodb://' +
+    config.get('Env.database.host') + ':' +
+    config.get('Env.database.port') + '/' +
     config.get('Env.database.name');
 
 logger.log('info', "starting up...");
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     var mongo = mongodb.MongoClient;
-    
-    mongo.connect(url, function(err, db){
-        if(err) 
-            logger.log('error', 'Failed to connect to database at ', url);
-        else
-            logger.log('info', "successfully connected to database at ", url);
 
-        db.close();
+    mongo.connect(url, function (err, db) {
+        if (err) {
+            logger.log('error', 'Failed to connect to database at ', url);
+        } else {
+            logger.log('info', "successfully connected to database at ", url);
+            db.close();
+        }
     });
 
     res.send(config.get('App.start_message') + 'from ' + os.hostname() + '\n');
